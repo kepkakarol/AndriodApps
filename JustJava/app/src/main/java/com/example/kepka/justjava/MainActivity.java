3dd1d05cd2016b2d3b48440a1579593dc07db2c6
kepka <karol.kepka@nsn.com>
2017-02-22 08:56:22 +0100

    add handle button

--- JustJava/app/src/main/java/com/example/kepka/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/kepka/justjava/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.kepka.justjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import java.text.NumberFormat;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+        int numberOfCoffees = 2;
+        int quantity = 5;
+        display(numberOfCoffees);
+        displayPrice(numberOfCoffees * quantity);
+    }
+
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
